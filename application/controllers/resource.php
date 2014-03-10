18ff8ce87c05157d1e4604ec9d28207e8ba9b160
Krish Sumaroo <ksumaroo@administrators-Mac-mini.local>
2014-03-10 16:43:14 -0400

    games controller

--- application/controllers/resource.php
+++ application/controllers/resource.php
@@ -4,9 +4,9 @@ class Resource extends CI_Controller {
 
     function  __construct(){
         parent::__construct();
-        
+
         //get userId from nonce
-        
+
         $this->userConnected = 1; //test value
 
     }
@@ -19,12 +19,12 @@ class Resource extends CI_Controller {
         //log_message('error', 'details =>'.print_r($result, true));
         //log_message('error', 'json =>'.  json_encode($result));
         $data['teamDetails'] = $result;
-       
+
         //print_r($result, true);
         $json = json_encode($data);
         $data['json'] = json_decode($json);
         echo $json;
-        
+
         $this->load->view('generic', $data);
     }
 
@@ -53,34 +53,40 @@ class Resource extends CI_Controller {
         $response = $this->user->activate('D123');
         echo json_encode(array('status' => $response));
     }
-    
+
     public function join()
     {
         $this->load->model('connection_model','connection');
-        //get from session -- uid is always the 1 who's logged in 
-        $uid = $this->userConnected; 
+        //get from session -- uid is always the 1 who's logged in
+        $uid = $this->userConnected;
         //$uid2 = 3;
         $uid2 = $this->input->post('uid');
-        
+
         //get hash for connection
         $connect = array('user1' => $uid, 'user2' => $uid2);
         $result = $this->connection->connect($connect);
         $this->_testResult($result);
     }
-    
+
     public function listConnections()
     {
         $this->load->model('connection_model','connection');
         $result = $this->connection->getConnections(array('user1' => $this->userConnected));
-        
+
         $jsonResponse = json_encode($result);
         echo $jsonResponse;
     }
-    
+
+    public function getMatches()
+    {
+        
+    }
+
+
     private function _testResult($result)
     {
         var_dump($result);
     }
 
-    
+
 }
