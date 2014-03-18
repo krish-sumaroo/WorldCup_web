d5ca008adc184b00bdab8b99b5131c046be66588
Krish Sumaroo <ksumaroo@administrators-Mac-mini.local>
2014-03-18 17:13:01 -0400

    added games

---
+++ application/models/game_model.php
@@ -0,0 +1,49 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class game_model extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = 'games';
+    }
+
+    public function getNext2Days()
+    {
+        $sql = "select m.id, m.stage, m.team1, m.team2, m.venue, m.matchDate from games  as m
+                INNER JOIN
+                (
+                        SELECT gameDate from gamesdates g
+                        where gamedate >= NOW()
+                        limit 2
+                ) as g1
+
+                ON m.matchDate = g1.`gameDate`
+                ORDER BY matchDate ASC";
+        $query = $this->db->query($sql);
+
+        if($query->num_rows() > 0)
+        {
+            $result = array();
+            foreach ($query->result() as $row)
+            {
+               $result[] = ['id' => $row->id,
+                            'stage' => $row->stage,
+                            'teamA' => $row->team1,
+                            'teamB' => $row->team2,
+                            'venue' => $row->venue,
+                            'matchDate' => $row->matchDate];
+            }
+            $this->db->close();
+            return $result;
+        }
+        else
+        {
+            //close connection
+            $this->db->close();
+            return FALSE;
+        }
+
+    }
+
+}
