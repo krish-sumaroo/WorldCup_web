7c43fc74f10a9fa7b1dd5250ac745004ae771f6b
krish-sumaroo <krish.sumaroo@gmail.com>
2014-06-13 08:51:34 +0000

    scores

--- application/config/mamouConfig.php
+++ application/config/mamouConfig.php
@@ -1,3 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['weekDays'] = array('Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
+$config['gameStatus'] = array(
+                           'not_started' => 0,
+                           'half1'    => 1,
+                           'half2'    => 2,  
+                           'gameOver'   => 3
+                    );
+
+$config['scorePoints'] = [50,40,35,30,25,20,10,5,2,2];
